c28783b869046d86d71123693c3c853bbbae123a
Gurnoorsb68 <gurnoorsb68@gmail.com>
2023-09-18 21:58:14 -0400

    implement using fastAPI

--- main.py
+++ main.py
@@ -1,36 +1,69 @@
-from flask import Flask, request
-
+import os
+from fastapi import FastAPI, File
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, UploadFile, HTTPException, Form
+from typing import Optional
 import boto3
+from botocore.exceptions import NoCredentialsError
 
-awstest2 = Flask(__name__)
+app = FastAPI()
 
-try:
-    s3 = boto3.client('s3')
-    lists = s3.list_buckets()
-    for x in lists["Buckets"]:
-        print(f'Bucket name is {x["Name"]}')
-except Exception as e:
-    print(f'An error occurred: {str(e)}')
+# Configure CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP headers
+)
 
-bucket_name = 'aiathelp'
 
-@awstest2.post('/upload-images')
-def upload_image():
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+S3_ACCESS_POINT = os.getenv('S3_ACCESS_POINT')
+S3_BUCKET = "aiathelp"
+
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, endpoint_url=S3_ACCESS_POINT)
+
+@app.get("/")
+async def read_root():
     try:
-        image_file = request.files['images']
-
-        if image_file:
-            object_key = 'imagestest/image.jpg'
-            s3.upload_fileobj(
-                Fileobj=image_file,
-                Bucket=bucket_name,
-                Key=object_key,
-            )
-
-            return {'message': ' Image successfully uploaded'}
-        else:
-            return {'error': 'No image file'}, 400
+        lists = s3.list_buckets()
+        bucket_names = [x["Name"] for x in lists["Buckets"]]
+        return {"message": "List of S3 buckets", "buckets": bucket_names}
+    except NoCredentialsError:
+        raise HTTPException(status_code=500, detail="AWS credentials not configured")
     except Exception as e:
-        return {'error': str(e)}, 500
+        raise HTTPException(status_code=500, detail=str(e))
 
-awstest2.run()
+@app.post("/upload/")
+async def upload_file(image_file: Optional[UploadFile] = File(...),username: str = Form(...)):
+    try:
+        if not image_file or image_file.filename == "":
+            raise HTTPException(status_code=400, detail="No image file provided")
+
+        # Generate the S3 key (path) where the file will be stored
+        s3_key = f"{username}/{image_file.filename}"
+        print(s3_key)  # Ensure that the s3_key is printed for debugging
+
+        # Upload the file to S3
+        s3.upload_fileobj(
+            Fileobj=image_file.file,
+            Bucket=S3_BUCKET,
+            Key=s3_key,
+        )
+
+        return {"message": "File uploaded successfully"}
+    except NoCredentialsError:
+        raise HTTPException(status_code=500, detail="AWS credentials not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
