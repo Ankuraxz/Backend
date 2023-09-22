ea972071550bbf5afac6375a1798f84acec4b423
ankuraxz <34418414+Ankuraxz@users.noreply.github.com>
2023-09-22 16:27:31 -0400

    Rename issue resolved for RGBA

--- main.py
+++ main.py
@@ -36,18 +36,31 @@ s3 = boto3.client('s3',
 dynamodb = boto3.client('dynamodb', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY,
                         region_name='us-east-1')
 
-def resize_image(image_file):
-    image = Image.open(image_file.file)
-    image.thumbnail((512, 512))
-    image_file.file = image
-    return image_file
+def resize_image(image_file, max_file_size_kb=100):
+    try:
+        with Image.open(io.BytesIO(image_file.file.read())) as img:
+            img.thumbnail((800, 800))
+
+            buffer = io.BytesIO()
+            img = img.convert('RGB')
+            img.save(buffer, format='JPEG')
+
+            file_size_kb = len(buffer.getvalue()) / 1024
+            if file_size_kb > max_file_size_kb:
+                raise HTTPException(status_code=400, detail="Resized image exceeds size limit") from None
+
+            buffer.seek(0)
+            return buffer
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error resizing image: {str(e)}") from e
 
 def rename_image(image_file, username):
-    # Making image name as UUID.jpg or UUID.png based on the image type
-    image_name = str(username+"_"+str(uuid.uuid4()))
+    image_name = str(username + "_" + str(uuid.uuid4()))
     image_type = image_file.filename.split(".")[-1]
+    # if no type, default to jpg
+    if len(image_type) == 0:
+        image_type = "jpg"
     image_file.filename = f"{image_name}.{image_type}"
-
 
 @app.get("/")
 def index():
@@ -65,27 +78,7 @@ async def read_root():
         raise HTTPException(status_code=500, detail=str(e)) from e
 
 
-def resize_image(image_file, max_file_size_kb=100):
-    try:
-        with Image.open(io.BytesIO(image_file.file.read())) as img:
-            img.thumbnail((800, 800))
 
-            buffer = io.BytesIO()
-            img.save(buffer, format='JPEG')
-
-            file_size_kb = len(buffer.getvalue()) / 1024
-            if file_size_kb > max_file_size_kb:
-                raise HTTPException(status_code=400, detail="Resized image exceeds size limit")
-
-            buffer.seek(0)
-            return buffer
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error resizing image: {str(e)}")
-
-def rename_image(image_file, username):
-    image_name = str(username + "_" + str(uuid.uuid4()))
-    image_type = image_file.filename.split(".")[-1]
-    image_file.filename = f"{image_name}.{image_type}"
 
 @app.post("/upload/")
 async def upload_file(image_file: UploadFile = File(...), username: str = Form(...)):
